8f90b3c5e9c26c27c3de75dd27aaa00ee0d1c831
TomasNagy <tomi.nagy@outlook.com>
2023-05-14 20:35:57 +0200

    Implement password checking function

--- password.c
+++ password.c
@@ -1,14 +1,16 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
-bool checkPass(string word, const char disablesCharArray[]);
+bool checkPass(string word, const string disablesCharArray);
 
 int main(void)
 {
 	// Array of not accepted characters, the last element of the array always should be a nul character (\0)
 	// The length of the array can be found by iterating through the array until reach the nul character (\0)
-	const char notAcceptedChars[] = {'#', '{', '}', '/', '~', '!', '$', '\0'};
+	const string notAcceptedChars = "#{}/~!$";
+
 
 	// Get the password from user
 	string password = get_string("Enter your password: ");
@@ -19,17 +21,48 @@ int main(void)
 	// Print information to user
 	if (isPassCorrect)
 	{
-		printf("Your password is valid!");
+		printf("Your password is valid!\n");
 		return 0;
 	}
 	else
 	{
-		printf("Your password needs at least one uppercase letter, lowercase letter, number and symbol!");
+		printf("Your password needs at least one uppercase letter, lowercase letter, number and symbol!\n");
 		return 1;
 	}
 }
 
-bool checkPass(string word, const char disablesCharArray[])
+bool checkPass(string word, const string disablesCharArray)
 {
-	return false;
+	bool lowerChar = false;
+	bool upperChar = false;
+	bool isNumber = false;
+
+	int notAcceptedCharLength = strlen(disablesCharArray);
+
+	for(int i = 0, length = strlen(word); i < length; i++)
+	{
+		for(int j = 0; j < notAcceptedCharLength; j++)
+		{
+			if(word[i] == disablesCharArray[j])
+			{
+				printf("There is an invalid character in your password!\n");
+				return false;
+			}
+		}
+
+		if(islower(word[i]) && lowerChar == false)
+		{
+			lowerChar = true;
+		}
+		else if(isupper(word[i]) && upperChar == false)
+		{
+			upperChar = true;
+		}
+		else if(isalnum(word[i]) && isNumber == false)
+		{
+			isNumber = true;
+		}
+	}
+
+	return lowerChar && upperChar && isNumber;
 }
