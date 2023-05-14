ed59a5d9f7947676c5f040457d5b08e189bdae4b
TomasNagy <tomi.nagy@outlook.com>
2023-05-14 19:53:34 +0200

    Add user input and feedback about the given password

--- password.c
+++ password.c
@@ -2,11 +2,30 @@
 #include <ctype.h>
 #include <stdio.h>
 
+bool checkPass(string word);
+
 int main(void)
 {
 	// Get the password from user
+	string password = get_string("Enter your password: ");
 
 	// Evaluate the password
+	bool isPassCorrect = checkPass(password);
 
 	// Print information to user
+	if (isPassCorrect)
+	{
+		printf("Your password is valid!");
+		return 0;
+	}
+	else
+	{
+		printf("Your password needs at least one uppercase letter, lowercase letter, number and symbol!");
+		return 1;
+	}
+}
+
+bool checkPass(string word)
+{
+	return false;
 }
